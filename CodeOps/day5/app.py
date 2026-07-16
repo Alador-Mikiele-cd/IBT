b95dde76912f5b350208d5c3ef40cb503d7f623e
Alador <aladormikiele.my20@gmail.com>
2026-07-16 03:23:18 -0700

    SavingsAccount and CurrentAccount classes added

--- CodeOps/day5/app.py
+++ CodeOps/day5/app.py
@@ -1,19 +1,110 @@
-class Animal:
-    def __init__(self,sound,name):
-        self.sound = sound
-    def speak(self):
-        return f'{self.sound}'
+# class Animal:
+#     def __init__(self,sound,name):
+#         self.sound = sound
+#     def speak(self):
+#         return f'{self.sound}'
     
-class dog(Animal):
-    def __init__(self, sound,bav):
-        super().__init__(sound)
-        self.bav = bav
+# class dog(Animal):
+#     def __init__(self, sound,bav):
+#         super().__init__(sound)
+#         self.bav = bav
 
-    def how(self):
-        return self.bav
+#     def how(self):
+#         return self.bav
 
-dog = dog("wooo",'loyal')
+# dog = dog("wooo",'loyal')
 
-print(dog.speak())
-print(dog.how())
-print(dog.sound)
+# print(dog.speak())
+# print(dog.how())
+# print(dog.sound)
+
+
+
+
+
+
+
+
+class Account:
+    def __init__(self,owner,account_no,bal):
+        self.owner = owner
+        self.account_no = account_no
+        self._bal = bal
+    
+    def deposit(self,amount):
+        if amount  <= 0:
+            raise ValueError("amount must be positive")
+        self._bal += amount
+        return f' you have deposited {amount} '
+    def withdraw(self,amount):
+        if amount  >= self._bal:
+            raise ValueError("amount must be less then you balnace")
+        self._bal -= amount
+        return f' you have withdraw {amount} '
+    @property
+    def statement(self):
+        return ( f"Account Type: Account\n"
+            f"Owner: {self.owner}\n"
+            f"Account No: {self.account_no}\n"
+            f"Balance: {self._bal}")
+    
+
+class SavingsAccount(Account):
+    def __init__(self, owner, account_no, bal,rate):
+        super().__init__(owner, account_no, bal)
+        self.rate = rate
+    def add_interest(self):
+        interest = self._bal * self.rate
+        self._bal += interest
+    @property
+    def statement(self):
+        return ( f"Account Type:Saving Account\n"
+            f"Owner: {self.owner}\n"
+            f"Account No: {self.account_no}\n"
+            f"Balance: {self._bal}")
+        
+    
+class CurrentAccount(Account):
+    def __init__(self, owner, account_no, bal,over):
+        super().__init__(owner, account_no, bal)
+        self.over = over
+
+    def withdraw(self,amount):
+        if amount  > self._bal + self.over:
+            raise ValueError("amount must be less then you balnace")
+        self._bal -= amount
+        return f' you have withdraw {amount} ' 
+    @property
+    def statement(self):
+        return ( f"Account Type:curreny Account\n"
+            f"Owner: {self.owner}\n"
+            f"Account No: {self.account_no}\n"
+            f"Balance: {self._bal}")   
+
+acc = Account("Alador", "CBE-10001", 1000)
+
+sav = SavingsAccount("Abel", "CBE-10002", 2000, 0.10)
+sav.add_interest()
+
+cur = CurrentAccount("John", "CBE-10003", 500, 1000)
+cur.withdraw(1200)
+
+accounts = [acc, sav, cur]
+
+for account in accounts:
+    print(account.statement)
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
